e8b5f65eab109996ba44c788eedb99bb3f59a447
Diana Gakuya <deegakuya@gmail.com>
2023-12-05 12:49:31 +0300

    complete

--- lib/regex.py
+++ lib/regex.py
@@ -3,3 +3,8 @@ import re
 my_pattern = r""
 my_regex = re.compile(my_pattern)
 
+import re
+
+# Define the my_regex pattern
+my_regex = re.compile(r"It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.")
+
